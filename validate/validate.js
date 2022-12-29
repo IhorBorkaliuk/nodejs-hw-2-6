4be94cf2c0588ba64025fc1acfdafea69b580488
Ihor Borkaliuk <happyviolence1990@gmail.com>
2022-12-29 19:56:24 +0100

    Update validate.js

--- validate/validate.js
+++ validate/validate.js
@@ -1,6 +1,7 @@
 const Joi = require("joi");
 
 const schema = Joi.object({
+  id: Joi.string().required(),
   name: Joi.string().min(3).max(30).required(),
   phone: Joi.string().min(10).max(10).required(),
   email: Joi.string().email({}).required(),
