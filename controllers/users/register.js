c53bf3edbb3bc49107ace144f9194ef4e0876aef
Ihor Borkaliuk <happyviolence1990@gmail.com>
2023-01-31 18:49:48 +0100

    in progress

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
+const { v4: uuidv4 } = require("uuid");
 const { User, validateRegisterSchema } = require("../../models/user");
+const sendEmail = require("../../services/sendEmail");
 
 const register = async (req, res, next) => {
   try {
@@ -13,17 +15,29 @@ const register = async (req, res, next) => {
     const userCheck = await User.findOne({ email });
     if (userCheck) {
       res.status(409).json({ message: "Email in use" });
+      return;
     }
 
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
     const avatar = gravatar.url(email);
+    const verificationToken = uuidv4();
 
     const newUser = await User.create({
       email,
       password: hashedPassword,
       avatarURL: avatar,
+      verificationToken,
     });
+
+    const verifyEmail = {
+      to: email,
+      subject: "Verify you email",
+      html: `<a target="_blank" 
+      href="http://localhost:3000/api/users/verify/${verificationToken}">Confirm your email></a>`,
+    };
+
+    await sendEmail(verifyEmail);
 
     res.status(201).json({
       status: "success",
